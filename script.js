de9ff68fd3cd0668d5d54fd40d344c548790e05e
Nexertc <ardevaalghifari80@gmail.com>
2025-10-28 05:13:54 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+function klik(){
+    document.getElementById("cont2").style.display = "flex";
+}
+function klik2(){
+  document.getElementById("cont2").style.display = "none"
+}
+
+function klik3(){
+    document.getElementById("cont3").style.display = "flex";
+}
+function klik4(){
+  document.getElementById("cont3").style.display = "none"
+}
+
+function klik5(){
+    document.getElementById("cont4").style.display = "flex";
+}
+function klik6(){
+  document.getElementById("cont4").style.display = "none"
+}
+
+
